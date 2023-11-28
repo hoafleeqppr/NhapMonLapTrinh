578c929147190bfb994c2e2526e0a07479331ac3
hoaflee.qppr <hoaminhle.104@gmail.com>
2023-11-28 13:34:05 +0700

    add circle file

---
+++ lesson/bt_1/circle.js
@@ -0,0 +1,6 @@
+const pi = 3.14 ; 
+let R = +prompt("nhap ban kinh R");
+let diameterOfCircle = 2*R*pi ;
+let acreageOfCircle = R*R*pi ;
+document.write("chu vi hinh tron la :" + " " + diameterOfCircle + "<br>");
+document.write("dien tich hinh tron la :" + " " + acreageOfCircle);
